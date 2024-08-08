faf0c8443401eed089c067576094d86dc1c4087f
Imelda Hope <imeldalarusso@gmail.com>
2024-08-08 06:29:22 +0300

    creates programmatic navigation using useNavigate hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const navigate = useNavigate();
 
+  const login = () => setIsLoggedIn(true);
+  const logout = () => setIsLoggedIn(false);
+
+  useEffect(() =>{
+    if (isLoggedIn) {
+        // navigates to Home route if user is logged in
+      navigate("/");
+    } else {
+        // navigates to Login route if user is logged out
+      navigate("/login");
+    };
+  }, [isLoggedIn]);
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      {isLoggedIn ? <NavBar logout={logout}  /> : <Navigate to="/login" />}
+      <Outlet context={login}/>
     </div>
   );
 }
